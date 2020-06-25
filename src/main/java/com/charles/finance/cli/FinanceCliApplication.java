e402439180b90f8df59a7a37c8beabf0a0a5b4b3
charles <dinneyacharles007@gmail.com>
2020-06-25 11:47:05 +0100

    added subcommand 'BalanceCommands'

--- src/main/java/com/charles/finance/cli/FinanceCliApplication.java
+++ src/main/java/com/charles/finance/cli/FinanceCliApplication.java
@@ -1,10 +1,12 @@
 package com.charles.finance.cli;
 
+import com.charles.finance.cli.commands.balance.BalanceCommands;
 import com.charles.finance.cli.commands.transaction.TransactionCommands;
 import picocli.CommandLine;
 
 @CommandLine.Command(subcommands = {
-        TransactionCommands.class
+        TransactionCommands.class,
+        BalanceCommands.class
 }, description = "CLI for consuming Finance Manager Webservice", name = "finance-cli")
 public class FinanceCliApplication implements Runnable {
 
