1554345db4ea50ae91eea44be3aa0b898247ecc9
charles <dinneyacharles007@gmail.com>
2020-06-25 11:55:43 +0100

    added Balance Domain

--- src/main/java/com/charles/finance/cli/domain/Balance.java
+++ src/main/java/com/charles/finance/cli/domain/Balance.java
@@ -1,4 +1,14 @@
 package com.charles.finance.cli.domain;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class Balance {
+
+    private Long id;
+    private LocalDate date;
+    private long amount;
+
 }
