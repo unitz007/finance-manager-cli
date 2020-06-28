f8a6a64dc5f695148f42833284da202b0b72b3d3
charles <dinneyacharles007@gmail.com>
2020-06-28 21:45:05 +0100

    abstract table display to Config.java and document codes

--- src/main/java/com/charles/finance/cli/commands/transaction/AllTransactionsCommand.java
+++ src/main/java/com/charles/finance/cli/commands/transaction/AllTransactionsCommand.java
@@ -2,10 +2,15 @@ package com.charles.finance.cli.commands.transaction;
 
 import com.charles.finance.cli.Config;
 import com.charles.finance.cli.domain.Transactions;
-import dnl.utils.text.table.TextTable;
 import org.springframework.web.client.RestTemplate;
 import picocli.CommandLine;
 
+/**
+ * defines command for querying all transactions
+ * @author Dinneya Charles
+ * @see java.lang.Runnable
+ * @version 1.0
+ */
 @CommandLine.Command(name = "--all", description = "Lists all Transactions")
 public class AllTransactionsCommand implements Runnable {
 
@@ -17,30 +22,7 @@ public class AllTransactionsCommand implements Runnable {
         Transactions[] transactions = restTemplate.getForObject(url, Transactions[].class);
         assert transactions != null;
 
-        String[] columns = {
-                "Date", "Category", "Type", "description", "Amount [#]"
-        };
-
-        int col = columns.length;
-        int row = transactions.length;
-
-        Object[][] data = new Object[row][col];
-
-        for (int i = 0; i < data.length; i++) {
-            for(int j = 0; j < data[i].length; j++) {
-                Transactions t = transactions[i];
-                data[i][0] = t.getDate();
-                data[i][1] = t.getCategory();
-                data[i][2] = t.getType();
-                data[i][3] = t.getDescription();
-                data[i][4] = t.getAmount();
-            }
-        }
-
-
-        TextTable table = new TextTable(columns, data);
-        table.setAddRowNumbering(true);
-        table.printTable();
-
+     // print Table
+        Config.transactionTable(transactions);
     }
 }
