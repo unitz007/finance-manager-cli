73af9b0586db56456adb970fa620cba3ec5c416d
charles <dinneyacharles007@gmail.com>
2020-06-29 14:09:43 +0100

    add comments

--- src/main/java/com/charles/finance/cli/commands/transaction/TransactionCommands.java
+++ src/main/java/com/charles/finance/cli/commands/transaction/TransactionCommands.java
@@ -3,6 +3,11 @@ package com.charles.finance.cli.commands.transaction;
 import com.charles.finance.cli.Config;
 import picocli.CommandLine;
 
+/**
+ * Entry point for transaction command
+ * @author Dinneya Charles
+ * @version 1.0
+ */
 @CommandLine.Command(name = "transaction", subcommands = {
         TransactionByTypeCommand.class,
         TransactionByDateCommand.class,
@@ -13,6 +18,10 @@ public class TransactionCommands implements Runnable {
 
     private String url = Config.baseUrl() + "/transactions";
 
+    /**
+     * method is invoked when transaction command is made
+     * displays help if sub-commands are not specified
+     */
     @Override
     public void run() {
             CommandLine cmd = new CommandLine(new TransactionCommands());
